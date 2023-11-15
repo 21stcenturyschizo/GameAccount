50df23ac7ac2f27c827961da56e0f558c357358b
21stcenturyschizo <149340147+21stcenturyschizo@users.noreply.github.com>
2023-11-15 10:47:00 +0100

    menu and GameAccount class functional

--- Program.cs
+++ Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        GameAccount currentUser = new GameAccount();
+
+        Console.WriteLine("Please enter your username:");
+        currentUser.userName = Console.ReadLine();
+
+        Console.WriteLine($"Welcome back, {currentUser.userName}!");
+        Console.WriteLine("Enter:");
+        Console.WriteLine("1 >>> Play a game");
+        Console.WriteLine("2 >>> See player stats");
+        Console.WriteLine("3 >>> Quit");
+
+        bool repeat = true;
+        Console.WriteLine("Please choose an option to proceed:");
+
+        do
+        {
+            string option = Console.ReadLine();
+            switch (option)
+            {
+                case "1":
+                    //
+                    Console.WriteLine("opt 1");
+                    repeat = false;
+                    break;
+                case "2":
+                    //
+                    Console.WriteLine("opt 2");
+                    repeat = false;
+                    break;
+                case "3":
+                    //
+                    Console.WriteLine("opt 3");
+                    repeat = false;
+                    break;
+
+            }
+            if (repeat) Console.WriteLine("Choose one of the options above.");
+        } while (repeat);
+
+    }
+}
+
+class GameAccount
+{
+    public string userName = "";
+    public int currentRating = 1;
+    public int gamesCount = 0;
+}
