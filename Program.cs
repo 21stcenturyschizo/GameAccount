561abe75aa4b0331db332f8b7eeec3c0d63aa6de
21stcenturyschizo <149340147+21stcenturyschizo@users.noreply.github.com>
2023-11-20 02:30:52 +0100

    GameStats fully functional + polished some other stuff

--- Program.cs
+++ Program.cs
@@ -3,9 +3,10 @@
     static void Main()
     {
         GameAccount currentUser = new GameAccount();
+        List<PlayedGame> PlayerStats = new List<PlayedGame>();
 
         Console.WriteLine("Please enter your username:");
-        currentUser.userName = Console.ReadLine();
+        currentUser.userName = Console.ReadLine() + "";
 
         Console.WriteLine($"Welcome back, {currentUser.userName}!");
 
@@ -16,14 +17,25 @@
             Console.WriteLine("1 >>> Play a game");
             Console.WriteLine("2 >>> See player stats");
             Console.WriteLine("3 >>> Quit");
-            string option = Console.ReadLine();
+            string option = Console.ReadLine() + "";
 
             switch (option)
             {
                 case "1":                    
-                    PlayGame(currentUser);
+                    PlayGame(currentUser, PlayerStats);
                     break;
-                case "2":                    
+                case "2": 
+                    if (currentUser.gamesCount != 0)
+                    {
+                    foreach (var game in PlayerStats)
+                    {
+                        Console.WriteLine($"Game number {game.gameIndex}" +
+                            $" against {game.opponentName}" +
+                            $" for {game.gameRating}" +
+                            $" rating points resulted in a {game.gameResult}.");
+                    }
+                    }
+                    else Console.WriteLine("No games played yet.");
                     break;
                 case "3":
                     Console.WriteLine("Quitting...");
@@ -37,41 +49,45 @@
         } while (repeat);
 
     }
-    static void PlayGame(GameAccount currentUser)
+    static void PlayGame(GameAccount currentUser, List<PlayedGame> PlayerStats)
     {
         GameAccount currentOpponent = GenerateOpponent(currentUser.currentRating);
         Console.WriteLine($"Your next opponent is \"{currentOpponent.userName}\" rated {currentOpponent.currentRating}.");
 
         Console.WriteLine("   ...feeling lucky? (Y/N)");
         bool repeat = true;
+        string result = "";
         do
         {
-            string result = Console.ReadLine();
-            switch(result)
+            string option = Console.ReadLine() + "";
+            switch(option)
             {
                 case "Y":
                     currentUser.currentRating = currentOpponent.currentRating;
+                    result = "win";
                     Console.WriteLine($"Wow, you won! Your rating is now {currentUser.currentRating}.");
                     repeat = false;
                     break;
                 case "N":
-                    Console.WriteLine($"Oh no, you lost. Your rating is now {currentUser.currentRating}.");
                     currentUser.currentRating -= (currentOpponent.currentRating - currentUser.currentRating) / 2;
                     if (currentUser.currentRating < 1) currentUser.currentRating = 1;
+                    result = "loss";
+                    Console.WriteLine($"Oh no, you lost. Your rating is now {currentUser.currentRating}.");
                     repeat = false;
                     break;
             }
         } while (repeat);
 
         currentUser.gamesCount++;
-
-
-        List<PlayedGame> PlayerStats = new List<PlayedGame>();
         PlayerStats.Add(new PlayedGame
         {
+            opponentName = currentOpponent.userName,
+            gameResult = result,
+            gameRating = currentOpponent.currentRating,
+            gameIndex = currentUser.gamesCount
+        }); ;
 
-        });
-
+        
     }
 
     static GameAccount GenerateOpponent(int userRating)
